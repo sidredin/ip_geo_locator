94a0539677335928f823bf0894bf2a4ca588042e
sadr3479 <sadr3479@gmail.com>
2022-03-05 18:33:39 +0300

    lesson 'Обработка и логирование ошибок' - refactored

--- services/Ip.php
+++ services/Ip.php
@@ -11,10 +11,10 @@ final class Ip
         if (empty($ip)) {
             throw new \InvalidArgumentException('Empty IP address');
         }
-        if (false === filter_var($ip, FILTER_VALIDATE_IP)) {
-            if (false === filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
-                throw new \InvalidArgumentException('Invalid IP address ' . $ip);
-            }
+        if (false === filter_var($ip, FILTER_VALIDATE_IP) &&
+            false === filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)
+        ) {
+            throw new \InvalidArgumentException('Invalid IP address ' . $ip);
         }
         $this->value = $ip;
     }
